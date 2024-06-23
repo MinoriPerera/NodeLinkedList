6e798b80b36f1e251db286690df140f4415f14b1
Minori <minoriperera1212@gmail.com>
2024-06-23 12:59:46 +0530

    Add files via upload

---
+++ LinkedList.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package datastructuresandalgorithms;
+
+/**
+ *
+ * @author Asus
+ */
+public class LinkedList {
+   
+    Node head;
+
+    public LinkedList() {
+        head = null;
+    }
+
+    // Add a node at the end of the list
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    // Delete a node with a specific value
+    public void delete(int data) {
+        if (head == null) {
+            System.out.println("List is empty.");
+            return;
+        }
+
+        if (head.data == data) {
+            head = head.next;
+            return;
+        }
+
+        Node current = head;
+        while (current.next != null && current.next.data != data) {
+            current = current.next;
+        }
+
+        if (current.next == null) {
+            System.out.println("Value not found in list.");
+        } else {
+            current.next = current.next.next;
+        }
+    }
+
+    // Display the list
+    public void display() {
+        if (head == null) {
+            System.out.println("List is empty.");
+            return;
+        }
+
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.display();
+
+        list.delete(20);
+        list.display();
+
+        list.delete(40);
+        list.display();
+    }
+}
+
+
